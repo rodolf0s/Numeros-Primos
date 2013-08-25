5ecb3e87dddfea232b03be8b0745d41cac5ee5f7
Rodolfo Santander <rodolfo.santander06@gmail.com>
2013-08-25 14:36:51 -0400

    Valida el ingreso de enteros

--- src/NumerosPrimos.java
+++ src/NumerosPrimos.java
@@ -6,11 +6,17 @@ public class NumerosPrimos {
 
 	public static void main(String[] args) {
 
+		int numero = 0;
 		in = new Scanner(System.in);
-		System.out.print("Ingrese numero: ");
-		int numero = in.nextInt();
 
-		System.out.println("Es primo? -> " + esPrimo(numero));
+		System.out.print("Ingrese numero: ");
+
+		try {
+			numero = in.nextInt();
+			System.out.println("Es primo? -> " + esPrimo(numero));
+		} catch (Exception e) {
+			System.out.println("No ingreso un numero entero");
+		}
 	}
 
 	public static boolean esPrimo(int n) {
